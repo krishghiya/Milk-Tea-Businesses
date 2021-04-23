5ba1c290ec4cd226d31e8237300e214e8f07378b
squach28 <squach3443@gmail.com>
2021-04-22 23:08:18 -0700

    added search bar functionalities

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { Component } from "react";
 import List from "@material-ui/core/List";
-
+import { Input, Button } from 'semantic-ui-react';
 import Business from "./components/Business";
 
 class App extends Component {
@@ -12,11 +12,16 @@ class App extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      items: []
+      items: [],
+      business: '',
+      location: '',
     }
+    this.handleBusinessChange = this.handleBusinessChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
-    fetch('https://ihxhi2nz68.execute-api.us-east-1.amazonaws.com/production?location=San%20Jose')
+    fetch('https://ihxhi2nz68.execute-api.us-east-1.amazonaws.com/production?location=San%20Jose') // TODO call backend api to find biggest hits
       .then(res => res.json())
       .then((result) => {
         console.log(result);
@@ -24,7 +29,32 @@ class App extends Component {
           isLoaded: true,
           items: result,
         });
-      })
+      });
+  }
+  // handles any changes that are made in the business input 
+  handleBusinessChange(event) {
+    this.setState({business: event.target.value});
+  }
+  // handles any changes that are made in the location input 
+  handleLocationChange(event) {
+    this.setState({location: event.target.value});
+  }
+
+  // handles when the search button is pressed
+  handleSubmit(event) {
+    alert('business: ' + this.state.business + ', location: ' + this.state.location);
+    event.preventDefault();
+    this.setState({isLoaded: false});
+    fetch('https://ihxhi2nz68.execute-api.us-east-1.amazonaws.com/production?location=' + this.state.location) // TODO call backend api 
+      .then(res => res.json())
+      .then((result) => {
+        console.log(result);
+        this.setState({
+          isLoaded: true,
+          items: result,
+        });
+      });
+
   }
 
   render() {
@@ -39,6 +69,11 @@ class App extends Component {
       return (
         <div className="App">
           <h1>Milk Tea Businesses</h1>
+          <form onSubmit={this.handleSubmit}>
+          <Input id='business' icon='coffee' iconPosition='left' placeholder='Business Name' value={this.state.value} onChange={this.handleBusinessChange}/>
+          <Input id='location' icon='location arrow' iconPosition='left' placeholder='City (e.g. San Jose, CA)' value={this.state.value} onChange={this.handleLocationChange}/>
+          <Button type='submit'>Search</Button>
+          </form>
           <List component="nav">
             {items.map(item => (<Business key={item.id} id={item.id} name={item.name} address={item.location.address1}/>))}
           </List>
