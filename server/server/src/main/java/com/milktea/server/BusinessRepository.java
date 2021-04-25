a6be75c108c685e4e922327dcaf7d02975c41439
Krish Ghiya <kghiya2@gmail.com>
2021-04-25 10:29:00 -0700

    Added BsuinessHits method

--- server/server/src/main/java/com/milktea/server/BusinessRepository.java
+++ server/server/src/main/java/com/milktea/server/BusinessRepository.java
@@ -2,9 +2,15 @@ package com.milktea.server;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BusinessRepository extends CrudRepository<Business, String> {
     
+    List<Business> findByName(String name);
+    List<Business> findByAddressLike(String address);
     List<Business> findByNameLikeAndAddressLike(String name, String address);
+    @Query("SELECT b.id, b.name, b.image_url, b.rating, b.review_count, b.display_phone, b.address"
+            +"FROM Businesses b JOIN BusinessHits USING (id)")
+    List<Business> findByHits();
 }
--- server/server/src/main/java/com/milktea/server/SearchController.java
+++ server/server/src/main/java/com/milktea/server/SearchController.java
@@ -1,5 +1,7 @@
 package com.milktea.server;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +16,23 @@ public class SearchController {
     private BusinessRepository repo;
 
     @GetMapping("/search")
-    public Business getBusinesses(@RequestParam String name, @RequestParam String location) {
-        
-        return new Business();
+    public List<Business> getBusinesses(@RequestParam(defaultValue = "") String name, 
+                                        @RequestParam(defaultValue = "") String location) {
+        List<Business> businesses;
+        if(name.equals("")) {
+            businesses = repo.findByAddressLike(location);
+        }
+        else if(location.equals("")) {
+            businesses = repo.findByName(name);
+        }
+        else{
+            businesses = repo.findByNameLikeAndAddressLike(name, location);
+        }
+        return businesses;
+    }
+
+    @GetMapping("/popular")
+    public List<Business> getBusinesses() {
+        return repo.findByHits();
     }
 }
