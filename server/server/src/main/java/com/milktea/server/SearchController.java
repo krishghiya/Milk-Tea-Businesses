2ee10aa972d3ab9d1ca0b20f9b09d200deb83632
Krish Ghiya <kghiya2@gmail.com>
2021-04-26 18:09:06 -0700

    removed unused dependency

--- server/server/src/main/java/com/milktea/server/SearchController.java
+++ server/server/src/main/java/com/milktea/server/SearchController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
-
 @CrossOrigin
 @RestController
 public class SearchController {
